5a995fdea3fd311360f11a9d79eee7b2165128fc
Indrajit <indrajitvijayakumar@gmail.com>
2020-05-23 00:48:57 +0530

    small changes to landing page

--- index.php
+++ index.php
@@ -83,7 +83,7 @@ include("session_check.php"); //checking whether to redirect the user to his/her
         <div class="container">
             <div class="row">
                 <div class="col-xl-9 mx-auto">
-                    <h1 class="text-capitalize text-center text-white mb-5">GET!SET!GO!</h1>
+                    <h1 class="text-capitalize text-center text-white mb-5">GET! SET! VROOM!</h1>
                 </div>
                 <div class="col-md-10 col-lg-8 col-xl-7 mx-auto">
                     <form action="register.html">
@@ -104,13 +104,13 @@ include("session_check.php"); //checking whether to redirect the user to his/her
                 <div class="col-lg-6 my-auto order-lg-1 showcase-text">
                     <h2 style="color: rgba(114,22,77,0.9);">Latest Deals!</h2>
                     <h2 style="color: rgba(0,0,0,1);">RUSTOM - the first buy limo-MARKET.</h2>
-                    <h4 style="color: rgba(0, 0, 0, 0.49);">Find all the latest car launches at incredible prices.</h4>
+                    <h4 style="color: rgba(0, 0, 0, 0.49);">Find all the latest cars at incredible prices.</h4>
                 </div>
             </div>
             <div class="row no-gutters">
                 <div class="col-lg-6 text-white showcase-img" style="background-image:url(&quot;assets/img/lt.jpg&quot;);"><span></span></div>
                 <div class="col-lg-6 my-auto order-lg-1 showcase-text">
-                    <h2 style="color: rgba(114,22,77,0.9);">Timeless!Luxury!Performance!</h2>
+                    <h2 style="color: rgba(114,22,77,0.9);">Timeless! Luxury! Performance!</h2>
                     <h2 style="color: rgba(0,0,0,1);">Enjoy your OPULENCE!</h2>
                     <h4 style="color: rgba(0, 0, 0, 0.49);">RUSTOM brings you all range of premium cars at your service.</h4>
                 </div>
@@ -120,7 +120,7 @@ include("session_check.php"); //checking whether to redirect the user to his/her
                 <div class="col-lg-6 my-auto order-lg-1 showcase-text">
                     <h2 style="color: rgba(114,22,77,0.9);">Best Assured!</h2>
                     <h2 style="color: rgba(0,0,0,1);">We CARE for your CAR!</h2>
-                    <h4 style="color: rgba(0, 0, 0, 0.49);">All Dealerships care about  their Customers but RUSTOM takes it to a whole new level.</h4>
+                    <h4 style="color: rgba(0, 0, 0, 0.49);">All dealerships care about their customers but RUSTOM takes it to a whole new level.</h4>
                 </div>
             </div>
         </div>
